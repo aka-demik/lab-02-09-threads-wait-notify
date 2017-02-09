2783b251d4b0eeac0b3ed42b1455beb7c62d3029
Денис Хлякин <mail4aka@gmail.com>
2017-02-09 12:06:08 +0300

    Initial import

---
+++ src/Chronos.java
@@ -0,0 +1,26 @@
+public class Chronos implements Runnable {
+    private final Object lock;
+    private int period;
+
+    public Chronos(Object lock, int period) {
+        this.lock = lock;
+        this.period = period;
+    }
+
+    @Override
+    public void run() {
+        long cm = System.currentTimeMillis();
+        while (true){
+            try {
+                Thread.sleep(period);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+            synchronized (lock) {
+                lock.notifyAll();
+                System.out.println("sec 1: " + (System.currentTimeMillis() - cm));
+            }
+        }
+    }
+}
---
+++ src/ChronosStrob.java
@@ -0,0 +1,30 @@
+public class ChronosStrob implements Runnable {
+    private final Object lock;
+    private final int strob;
+    private int counter = 0;
+
+    public ChronosStrob(Object lock, int strob) {
+        this.lock = lock;
+        this.strob = strob;
+    }
+
+    @Override
+    public void run() {
+        long cm = System.currentTimeMillis();
+
+        while (true) {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                    ++counter;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+
+                }
+            }
+            if ((counter % strob) == 0) {
+                System.out.println("sec 5: " + (System.currentTimeMillis() - cm));
+            }
+        }
+    }
+}
